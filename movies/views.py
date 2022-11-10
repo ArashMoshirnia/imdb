4328195108fff0bb0b3348fc2c200ccaf6f5180f
Arash Moshirnia <arash_moshirnia@yahoo.com>
2022-11-10 09:30:59 +0330

    Move movie create logic to movies_list view

--- movies/views.py
+++ movies/views.py
@@ -6,13 +6,23 @@ from movies.forms import MovieForm
 
 
 def movies_list(request):
-    movies = Movie.objects.all()[:8]
-    context = {
-        "movies": movies,
-        "user": "Arash",
-        "is_valid": True
-    }
-    return render(request, 'movies/movies_list.html', context=context)
+    if request.method == 'GET':
+        movies = Movie.objects.all()[:8]
+        context = {
+            "movies": movies,
+            "user": "Arash",
+            "is_valid": True
+        }
+        return render(request, 'movies/movies_list.html', context=context)
+
+    elif request.method == 'POST':
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('movies_list')
+
+        return movie_add(request, form)
 
 
 def movie_detail(request, pk):
@@ -20,30 +30,6 @@ def movie_detail(request, pk):
 
 
 def movie_add(request, movie_form=None):
-    if request.method == 'GET':
-        if not movie_form:
-            movie_form = MovieForm()
-        return render(request, 'movies/movie_add.html', context={'form': movie_form})
-
-    elif request.method == 'POST':
-        form = MovieForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-            # title = form.cleaned_data.get('title')
-            # description = form.cleaned_data.get('description')
-            # release_date = form.cleaned_data.get('release_date')
-            # avatar = form.cleaned_data.get('avatar')
-            #
-            # Movie.objects.create(
-            #     title=title,
-            #     description=description,
-            #     release_date=release_date,
-            #     avatar=avatar
-            # )
-
-            # Redirect
-            return redirect('movies_list')
-
-        request.method = 'GET'
-        return movie_add(request, form)
+    if not movie_form:
+        movie_form = MovieForm()
+    return render(request, 'movies/movie_add.html', context={'form': movie_form})

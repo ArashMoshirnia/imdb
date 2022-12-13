0b6ff597d54444a4e1c8ff664322db27c9545cfd
Arash Moshirnia <arash_moshirnia@yahoo.com>
2022-12-13 21:00:17 +0330

    Add redis

--- imdb/settings.py
+++ imdb/settings.py
@@ -143,8 +143,8 @@ DATABASES = {
 # Cache
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
         'KEY_PREFIX': 'imdb',
         'TIMEOUT': None
     },
